1db4a1fdaf17b418f1b3197ea5249bd41b89524f
RakovJavaAQA <rakovaqa@yandex.ru>
2025-07-05 21:34:39 +0100

    Добавил генератор пользователя под разные сценарии

---
+++ src/main/java/UserGenerator.java
@@ -0,0 +1,48 @@
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class UserGenerator {
+    //Рандомный пользователь со всеми полями
+    public User randomUser() {
+        String email = RandomStringUtils.randomAlphabetic(5) + "@" + RandomStringUtils.randomAlphabetic(5) + ".ru";
+        String password = RandomStringUtils.randomNumeric(8);
+        String name = RandomStringUtils.randomAlphabetic(8);
+        return new User(email, password, name);
+    }
+    //Рандомный пользователь без mail
+    public User randomUserWithoutEmail() {
+        String password = RandomStringUtils.randomNumeric(8);
+        String name = RandomStringUtils.randomAlphabetic(8);
+        return new User(null, password, name);
+    }
+    //Рандомный пользователь без password
+    public User randomUserWithoutPassword() {
+        String email = RandomStringUtils.randomAlphabetic(5) + "@" + RandomStringUtils.randomAlphabetic(5) + ".ru";
+        String name = RandomStringUtils.randomAlphabetic(8);
+        return new User(email, null, name);
+    }
+    //Рандомный пользователь без name
+    public User randomUserWithoutName() {
+        String email = RandomStringUtils.randomAlphabetic(5) + "@" + RandomStringUtils.randomAlphabetic(5) + ".ru";
+        String password = RandomStringUtils.randomNumeric(8);
+        return new User(email, password, null);
+    }
+    //Рандомный пользователь с неверным email
+    public User randomUserWithWrongEmail() {
+        String email = RandomStringUtils.randomNumeric(8);
+        String password = RandomStringUtils.randomNumeric(8);
+        return new User(email, password, null);
+    }
+
+    //ля логина
+    public User forLogin(String email, String password) {
+        return new User(email, password, null);
+    }
+    //Для логина с неверным email и password
+    public User forWrongLogin(String email, String password) {
+        return new User("1"+email, "2"+password, null);
+    }
+    //Для изменения данных пользователя
+    public User forChangedUser (String email, String password, String name) {
+        return new User(email, password, name);
+    }
+}
